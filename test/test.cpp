62374da693f519a2e824096d4383b63b658e7796
GuiLi <qligui@gmail.com>
2021-01-06 11:31:45 +0800

    增加单元测试模块

---
+++ test/test.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include <sw-protocol/sw_protocol.hpp>
+#include <test/catch.hpp>
+
+TEST_CASE("json func test")
+{
+	REQUIRE(2 == 2);
+}
+TEST_CASE("xml func test")
+{
+	REQUIRE(2 == 2);
+}
